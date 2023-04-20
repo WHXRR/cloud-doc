2289808e6dd0a204a200a19692d608f9cbf13a6b
WXHRR <1147232219@qq.com>
2023-04-20 11:24:31 +0800

    添加导入功能

--- src/App.js
+++ src/App.js
@@ -8,13 +8,15 @@ import TabList from './components/tabList/TabList';
 import Editor from './components/editor/Editor';
 import { useEffect, useState } from 'react';
 import { v4 as uuidv4 } from 'uuid';
-import { objToArr } from './utils/helper'
+import { flattenArr, objToArr } from './utils/helper'
 import fileHelper from './utils/fileHelper';
 import { Modal } from 'antd';
 
-const { join } = window.require('path')
-const { app } = window.require('@electron/remote')
+const { join, basename } = window.require('path')
+const { app, dialog } = window.require('@electron/remote')
 
+// 存储路径
+const saveLocation = app.getPath('documents')
 const Store = window.require('electron-store');
 const fileStore = new Store({ 'name': 'Files Data' });
 const saveFilesToStore = (files) => {
@@ -29,8 +31,6 @@ const saveFilesToStore = (files) => {
   }, {})
   fileStore.set('files', filesObj)
 }
-// 存储路径
-const saveLocation = app.getPath('documents')
 
 function App() {
   const [files, setFiles] = useState(fileStore.get('files') || {})
@@ -67,6 +67,7 @@ function App() {
         // 手动删除本地文件后，会找不到文件导致报错
         setOpen(true);
         setErrorFileId(data.id)
+        // handleErrorFile()
       })
     }
     setActiveFileId(data.id)
@@ -86,6 +87,7 @@ function App() {
       }).catch(() => {
         setOpen(true);
         setErrorFileId(data.id)
+        // handleErrorFile()
       })
       // 如果打开的文件列表中有当前文件，则更新打开的文件列表
       if (openedFileIds.includes(data.id)) {
@@ -115,6 +117,7 @@ function App() {
       }).catch(() => {
         setOpen(true);
         setErrorFileId(id)
+        // handleErrorFile()
       })
     }
   }
@@ -157,7 +160,50 @@ function App() {
     }
     setFiles({ ...files, [id]: newFile })
   }
-  const onImportFile = () => { }
+  const onImportFile = () => {
+    dialog.showOpenDialog({
+      title: '请选择要导入的 MarkDown 文件',
+      properties: ['openFile', 'multiSelections'],
+      filters: [
+        { name: 'MarkDown files', extensions: ['md'] },
+      ]
+    }).then(res => {
+      if (!res.canceled) {
+        const newFiles = res.filePaths.map(path => {
+          // 获取导入的文件的信息
+          let fileName = basename(path)
+          // 相同名字的在文件名后面加索引
+          const sameFileNameArr = filesArr.filter(file => file.name === fileName)
+          if (sameFileNameArr.length) {
+            fileName = `${fileName.split('.md')[0]}(${sameFileNameArr.length}).md`
+          }
+          const id = uuidv4()
+          const newPath = join(saveLocation, fileName)
+          fileHelper.readFile(path).then((fileBody) => {
+            fileHelper.writeFile(newPath, fileBody)
+          })
+          return {
+            id,
+            path: newPath,
+            name: fileName,
+            createdAt: new Date().getTime(),
+            isNew: false
+          }
+        })
+        // 读取导入文件的内容
+        setFiles({ ...files, ...flattenArr(newFiles) })
+        saveFilesToStore({ ...files, ...flattenArr(newFiles) })
+      }
+    }).catch(err => {
+      dialog.showMessageBox({
+        message: err,
+        title: '警告',
+        type: 'error',
+      }).then(result => {
+        console.log(result);
+      })
+    })
+  }
   const handleSave = () => {
     fileHelper.writeFile(join(saveLocation, `${activeFile.name}.md`), activeFile.body).then(() => {
       const newUnSavedIds = unSavedIds.filter(id => id !== activeFileId)
@@ -173,6 +219,22 @@ function App() {
     saveFilesToStore(afterDelete)
     setOpen(false);
   };
+
+  const handleErrorFile = () => {
+    dialog.showMessageBox({
+      message: '未找到该文件，是否删除？',
+      title: '提示',
+      type: 'info',
+      buttons: ['取消', '确认']
+    }).then(result => {
+      if (!result.response) return
+      const { [errorFileId]: value, ...afterDelete } = files
+      setFiles(afterDelete)
+      saveFilesToStore(afterDelete)
+      setOpen(false);
+    })
+  }
+
   return (
     <div className="App container-fluid px-0">
       <div className='row g-0'>
